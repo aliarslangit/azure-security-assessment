14cf834c0083d576f12a07ebdcf467f47b9ad55e
aliarslangit <aliarslanmushtaqali@gmail.com>
2022-05-14 14:30:55 +0500

    Update assessment.ps1

--- assessment.ps1
+++ assessment.ps1
@@ -1,5 +1,14 @@
-//Get all Nsgs in a subscription
 $sub = Get-AzSubscription -SubscriptionId "" | Set-AzContext
 $nsgs = Get-AzNetworkSecurityGroup
 Get-AzAdvisorRecommendation
+
+foreach ( $azNsg in $azNsgs ) {
+    Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $azNsg | Where-Object { $_.sourceaddressprefix -eq "*" -and $_.Access -eq "Allow" } | `
+    Select-Object @{label = 'NSG Name'; expression = { $azNsg.Name } }, @{label = 'Rule Name'; expression = { $_.Name } }, `
+    @{label = 'Source'; expression = { $_.SourceAddressPrefix } }, `
+    @{label = 'Destination'; expression = { $_.DestinationAddressPrefix } }, `
+    @{label = 'Port Range'; expression = { $_.DestinationPortRange } }, Access, Priority, Direction, `
+    @{label = 'Resource Group Name'; expression = { $azNsg.ResourceGroupName } } | Export-Csv -Path "$($home)\clouddrive\nsg-audit.csv" -NoTypeInformation -Append
+  
+}
 
